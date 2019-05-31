bb3dca06886754cb1dcaf652f6a3eef648c4ec34
Tyler Bray <broncobray77@gmail.com>
2019-05-31 15:03:21 -0700

    video detail added

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../API/youtube';
 import VideoList from '../components/VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
 
-  state = { videos: [] }
+  state = { videos: [], videoSelected: null }
 
   onTermSubmit = async term => {
     const response = await youtube.get('/search', {
@@ -20,11 +21,16 @@ class App extends React.Component {
     })
   };
 
+  onVideoSelect = (video) => {
+    this.setState({videoSelected: video})
+  }
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit}/>
-        <VideoList videos={this.state.videos}/>
+        <VideoDetail video={this.state.videoSelected}/>
+        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
       </div>
     )
   }
