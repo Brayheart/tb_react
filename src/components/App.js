d48f16bca8f263ab4e7250dafe0e59462eec4bab
Tyler Bray <broncobray77@gmail.com>
2019-06-03 14:39:03 -0700

    styling

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,11 @@ class App extends React.Component {
 
   state = { videos: [], videoSelected: null }
 
+
+  componentDidMount() {
+    this.onTermSubmit('buildings');
+  }
+
   onTermSubmit = async term => {
     const response = await youtube.get('/search', {
       params: {
@@ -17,7 +22,8 @@ class App extends React.Component {
 
 
     this.setState({
-      videos: response.data.items
+      videos: response.data.items,
+      videoSelected: response.data.items[0]
     })
   };
 
@@ -29,8 +35,16 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit}/>
-        <VideoDetail video={this.state.videoSelected}/>
-        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+        <div className='ui grid'>
+          <div className='ui row'>
+            <div className='eleven wide column'>
+              <VideoDetail video={this.state.videoSelected}/>
+            </div>
+            <div className='five wide column'>
+              <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
